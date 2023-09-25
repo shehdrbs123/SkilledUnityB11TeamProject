cfeb1a67c9803d3155d6161d454e0abac5fc41b1
tjdgh7419 <70570791+tjdgh7419@users.noreply.github.com>
2023-09-25 17:30:53 +0900

    [ADD] Test에셋추가

--- SkilledUnityB11TeamProject/Assets/Scriptables/Scripts/ItemData.cs
+++ SkilledUnityB11TeamProject/Assets/Scriptables/Scripts/ItemData.cs
@@ -2,17 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemData : MonoBehaviour
+public enum ItemType
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	Resource,
+	Equipable,
+	Consumable
+}
+public enum ConsumableType
+{
+    Hunger,
+	Thirst
+}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+[CreateAssetMenu(fileName = "Item", menuName = "New Item")]
+public class ItemData : ScriptableObject
+{
+	[Header("Info")]
+	public string itemName;
+	public string description;
+	public ItemType type;
+	public Sprite icon;
+	public GameObject dropPrefab;
+
+	[Header("Stacking")]
+	public bool canStack;
+	public int maxStackAmount;
 }
