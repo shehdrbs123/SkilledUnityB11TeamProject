6542cce2ce5145049e07e2dee3b91e5cb5ff705f
shehdrbs123 <shehdrbs123@naver.com>
2023-09-25 16:45:37 +0900

    [ADD] 플레이어 Conditions 완성

--- SkilledUnityB11TeamProject/Assets/Scripts/Components/Conditions.cs
+++ SkilledUnityB11TeamProject/Assets/Scripts/Components/Conditions.cs
@@ -6,31 +6,77 @@ using UnityEngine;
 public enum ConditionType{
     Mental,Hunger,Thirsty,Health
 }
-
+[Serializable]
 public class Condition
 {
+    public ConditionType type;
     public float curValue;
-    public float MaxValue;
-    public float StartValue;
+    public float maxValue;
+    public float startValue;
     public float decayRate;
     public float regenRate;
 
     public event Action OnValueChanged;
 
+    private float decayPerSeconds;
+    private float regenPerSeconds;
+
+    public void Init()
+    {
+        curValue = startValue;
+        decayPerSeconds = decayRate * Time.deltaTime;
+        regenPerSeconds = regenRate * Time.deltaTime;
+    }
+    public void Update()
+    {
+        Decay();
+        Regen();
+        OnValueChanged?.Invoke();
+    }
+
+    private void Regen()
+    {
+        Add(regenPerSeconds);
+    }
+
+    private void Decay()
+    {
+        Add(-decayPerSeconds);
+    }
+
     public void Add(float amount)
     {
-        curValue = Mathf.Clamp(curValue + amount,0, MaxValue);
-        OnValueChanged?.Invoke();
+        curValue = Mathf.Clamp(curValue + amount,0, maxValue);
     }
 
     public float GetPercentage()
     {
-        return curValue / MaxValue;
+        return curValue / maxValue;
     }
 }
 public class Conditions : MonoBehaviour
 {
-    public Dictionary<ConditionType, Condition> conditions = new Dictionary<ConditionType, Condition>();
+    [Header("능력치 선택")]
+    [SerializeField] private Condition[] _listConditions;
+    private Dictionary<ConditionType, Condition> conditions = new Dictionary<ConditionType, Condition>();
+
+    private void Awake()
+    {
+        conditions = new Dictionary<ConditionType, Condition>();
+        foreach (var condition in _listConditions)
+        {
+            conditions.Add(condition.type,condition);
+            condition.Init();
+        }
+    }
+
+    private void Update()
+    {
+        foreach (var condition in _listConditions)
+        {
+            condition.Update();
+        }
+    }
 
     public Condition GetCondition(ConditionType type)
     {
@@ -43,5 +89,11 @@ public class Conditions : MonoBehaviour
 #endif
         }
         return value;
+    }
+
+    public void AddCondition(ConditionType type, int amount)
+    {
+        Condition con = GetCondition(type);
+        con.Add(amount);
     }
 }
