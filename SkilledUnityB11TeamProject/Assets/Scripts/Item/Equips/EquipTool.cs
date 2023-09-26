81b7d8d2d6a039b330432c71a2290b10bee6cc5f
tjdgh7419 <70570791+tjdgh7419@users.noreply.github.com>
2023-09-26 15:55:11 +0900

    [ADD] 자원 채취기능 추가

--- SkilledUnityB11TeamProject/Assets/Scripts/Item/Equips/EquipTool.cs
+++ SkilledUnityB11TeamProject/Assets/Scripts/Item/Equips/EquipTool.cs
@@ -43,23 +43,18 @@ public class EquipTool : Equip
 		}
 	}
 
-	//public void OnHit()
-	//{
-	//	Ray ray = camera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
-	//	RaycastHit hit;
+	public void OnHit()
+	{
+		Ray ray = camera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+		RaycastHit hit;
 
-	//	if (Physics.Raycast(ray, out hit, attackDistance))
-	//	{
-	//		if (doesGatherResources && hit.collider.TryGetComponent(out Resource resource))
-	//		{
-	//			resource.Gather(hit.point, hit.normal);
-	//		}
-
-	//		if (doesDealDamage && hit.collider.TryGetComponent(out IDamagable damageable))
-	//		{
-	//			damageable.TakePhysicalDamage(damage);
-	//		}
-	//	}
-	//}
+		if (Physics.Raycast(ray, out hit, attackDistance))
+		{
+			if (doesGatherResources && hit.collider.TryGetComponent(out Resource resource))
+			{
+				resource.Gather();
+			}
+		}
+	}
 
 }
