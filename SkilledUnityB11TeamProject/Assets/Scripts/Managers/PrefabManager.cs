8541e8666aa90d884ad1b18676285772d8baab82
minsang <minsang0610@naver.com>
2023-09-26 20:57:36 +0900

    [UPDATE] PrefabManager
    GameManager 프리팹 수정

--- SkilledUnityB11TeamProject/Assets/Scripts/Managers/PrefabManager.cs
+++ SkilledUnityB11TeamProject/Assets/Scripts/Managers/PrefabManager.cs
@@ -8,6 +8,13 @@ public struct Pool
     public PoolType type;       // 풀링할 오브젝트
     public GameObject prefab;   // 프리팹
     public Transform holder;    // 오브젝트 생성 부모. PrefabMangaer 컴포넌트 아래에 새 게임오브젝트를 만들어서 달아주기. Hierarchy 정리용
+
+    public Pool(PoolType _type, GameObject _prefab, Transform _holder)
+    {
+        type = _type;
+        prefab = _prefab;
+        holder = _holder;
+    }
 }
 
 public enum PoolType            // PoolType의 값만큼 오브젝트 풀링
@@ -25,17 +32,41 @@ public class PrefabManager : MonoBehaviour
     {
         foreach (Pool pool in pools)
         {
-            Queue<GameObject> objectPool = new Queue<GameObject>();
-
-            for (int i = 0; i < (int)pool.type; i++)
-            {
-                GameObject _object = Instantiate(pool.prefab, pool.holder);
-                _object.SetActive(false);
-                objectPool.Enqueue(_object);
-            }
-
-            poolDictionary.Add(pool.type, objectPool);
+            AddOnDictionary(pool);
         }
+    }
+
+    private void AddOnDictionary(Pool pool)
+    {
+        Queue<GameObject> objectPool = new Queue<GameObject>();
+
+        for (int i = 0; i < (int)pool.type; i++)
+        {
+            GameObject _object = Instantiate(pool.prefab, pool.holder);
+            _object.SetActive(false);
+            objectPool.Enqueue(_object);
+        }
+
+        poolDictionary.Add(pool.type, objectPool);
+    }
+
+    public void AddAtPoolsOnScript(PoolType _type, GameObject _object)      // holder 지정해주지 않으면 GameManager.PrefabManager 하위에 홀더가 생김
+    {
+        Transform tf = new GameObject("Holder").transform;
+        tf.SetParent(transform);
+
+        Pool newPool = new Pool(_type, _object, tf);
+
+        pools.Add(newPool);
+        AddOnDictionary(newPool);
+    }
+
+    public void AddAtPoolsOnScript(PoolType type, GameObject obj, Transform holder)
+    {
+        Pool newPool = new Pool(type, obj, holder);
+
+        pools.Add(newPool);
+        AddOnDictionary(newPool);
     }
 
     public GameObject SpawnFromPool(PoolType type)
@@ -45,6 +76,8 @@ public class PrefabManager : MonoBehaviour
 
         GameObject _object = poolDictionary[type].Dequeue();
         poolDictionary[type].Enqueue(_object);
+
+        _object.SetActive(true);
 
         return _object;
     }
