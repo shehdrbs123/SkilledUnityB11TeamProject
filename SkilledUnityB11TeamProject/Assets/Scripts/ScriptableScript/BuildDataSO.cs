1d9b8c7e9bd92e79bdfed292896b9ab1ae881a34
shehdrbs123 <shehdrbs123@naver.com>
2023-09-26 14:37:46 +0900

    [ADD] scriptable Object 생성, 빌드 데이터, 이미지 등 추가

---
+++ SkilledUnityB11TeamProject/Assets/Scripts/ScriptableScript/BuildDataSO.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "BuildDataSO", menuName = "Scriptable/Build Data")]
+public class BuildDataSO : ScriptableObject
+{
+    public Sprite StructureUISprite;
+    public GameObject StructurePrefabs;
+    
+    //private ItemData[] resoureces
+    //private int[] resourecsCount;
+}
