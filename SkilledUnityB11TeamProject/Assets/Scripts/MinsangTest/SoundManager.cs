0ca21eccee6c706b44bff6fc87ba91ded0ee5ca9
minsang <minsang0610@naver.com>
2023-09-26 14:51:15 +0900

    [ADD] SoundManager

--- SkilledUnityB11TeamProject/Assets/Scripts/MinsangTest/SoundManager.cs
+++ SkilledUnityB11TeamProject/Assets/Scripts/MinsangTest/SoundManager.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static SoundManager instance;        // 후에 gameManager 사용하면서 싱글톤 해제
+
+    [SerializeField] [Range(0f, 1f)] private float soundEffectVolume;
+    [SerializeField] [Range(0f, 1f)] private float soundEffectPitchVariance;
+    [SerializeField] [Range(0f, 1f)] private float bgmVolume;
+
+    private PrefabManager prefabManager;
+
+    private AudioSource bgmAudioSource;
+    public AudioClip bgmClip;
+
+    private void Awake()
     {
-        
+        instance = this;
+
+        bgmAudioSource = GetComponent<AudioSource>();
+        bgmAudioSource.volume = bgmVolume;
+        bgmAudioSource.loop = true;
+
+        prefabManager = GetComponent<PrefabManager>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        ChangeBackGroundMusic(bgmClip);
+    }
+
+    public static void ChangeBackGroundMusic(AudioClip music)
+    {
+        instance.bgmAudioSource.Stop();
+        instance.bgmAudioSource.clip = music;
+        instance.bgmAudioSource.Play();
+    }
+
+    public static void PlayClip(AudioClip clip)
+    {
+        GameObject obj = instance.prefabManager.SpawnFromPool(PoolType.SFXAudio);
+        obj.SetActive(true);
+        obj.GetComponent<SoundSource>().Play(clip, instance.soundEffectVolume, instance.soundEffectPitchVariance);
     }
 }
