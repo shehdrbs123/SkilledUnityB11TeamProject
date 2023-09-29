4be31ecc4e1b32f255438a7841563b3ee2d4a1b1
tjdgh7419 <70570791+tjdgh7419@users.noreply.github.com>
2023-09-29 21:49:06 +0900

    [UPDATE]TestScene 수정

--- SkilledUnityB11TeamProject/Assets/Scripts/Components/Player/EquipTool.cs
+++ SkilledUnityB11TeamProject/Assets/Scripts/Components/Player/EquipTool.cs
@@ -45,11 +45,11 @@ public class EquipTool : Equip
 		
 		Ray ray = camera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
 		RaycastHit hit;
-
+		
 		if (Physics.Raycast(ray, out hit, attackDistance))
 		{
 			if (doesGatherResources && hit.collider.TryGetComponent(out Resource resource))
-			{
+			{ 
 				GameManager.Instance.ResourceDisplayUI.resourceTxt.text = resource.itemToGive.itemName;
 				GameManager.Instance.ResourceDisplayUI.resourceDisplayImg.SetActive(true);
 				GameManager.Instance.ResourceDisplayUI.animator.SetTrigger("OnCollect");
