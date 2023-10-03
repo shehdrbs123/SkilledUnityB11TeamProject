4a51f12ef243e9281c60fe6956dd995698da57d0
shehdrbs123 <shehdrbs123@naver.com>
2023-10-03 23:55:39 +0900

    [TIDY] 파일 정리(외부 에셋 한곳에 모으기)

--- SkilledUnityB11TeamProject/Assets/Scripts/UI/ResourceDisplayUI.cs
+++ SkilledUnityB11TeamProject/Assets/Scripts/UI/ResourceDisplayUI.cs
@@ -4,7 +4,7 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ResourceDisplayUI : MonoBehaviour
+public class ResourceDisplayUI: MonoBehaviour
 {
 	PrefabManager prefabManager;
 
